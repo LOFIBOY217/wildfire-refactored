8310e2632bba490fe14fb5aa0a2b7c08fd62bbcb
LOFIBOY217 <jiaaqii.huang@mail.utoronto.ca>
2026-03-31 18:26:59 -0400

    fix: increase cache build mem to 256G

--- slurm/build_s2s_full_patch_cache.sh
+++ slurm/build_s2s_full_patch_cache.sh
@@ -3,7 +3,7 @@
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=16
-#SBATCH --mem=128G
+#SBATCH --mem=256G
 #SBATCH --time=24:00:00
 #SBATCH --output=/scratch/jiaqi217/logs/build_s2s_cache_%j.out
 #SBATCH --error=/scratch/jiaqi217/logs/build_s2s_cache_%j.err
